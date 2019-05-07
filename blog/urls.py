09a8cd16c206b4626e1bf9cffb42755a8dc25b29
pinguingman <pinguingman@gmail.com>
2019-05-07 21:01:01 +0300

    Add basic templates.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-
+app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^subscriptions$', views.SubscriptionsView.as_view(), name='subscriptions_page'),
+    url(r'^user/(?P<username>\w+)/$', views.UserView.as_view(), name='user_page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, TemplateView, View
+
+from blog.models import Blog_record
+
+
+class IndexView(View):
+    def get(self, request):
+        records = Blog_record.objects.all().order_by('-creation_date')
+        return render(request, 'blog/index.html', {
+            'records': records
+        })
+
+
+class UserView(View):
+    def get(self, request, username):
+        records = Blog_record.objects.filter(author__username=username).order_by('-creation_date')
+        return render(request, 'blog/user_page.html', {
+            'records': records
+        })
+
+
+class SubscriptionsView(View):
+    def get(self, request):
+        return HttpResponse('Current user subscriptions')
 
 
 def index(request):
-    return HttpResponse('Test')
+    return HttpResponse('index')
