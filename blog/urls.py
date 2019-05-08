20d33fb95278e36f8a2624f78de8cef69fd494c1
pinguingman <pinguingman@gmail.com>
2019-05-08 17:51:10 +0300

    Add view and html templates for blog.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^subscriptions/$', views.SubscriptionsView.as_view(), name='subscriptions_page'),
+    url(r'^my_page/$', views.BlogView.as_view(), name='user_blog'),
     url(r'^user/(?P<username>\w+)/$', views.UserView.as_view(), name='user_page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, TemplateView, View
 
-from blog.models import Blog_record
+from blog.models import Blog_record, User
 
 
 class IndexView(View):
     def get(self, request):
         records = Blog_record.objects.all().order_by('-creation_date')
         return render(request, 'blog/index.html', {
-            'records': records
+            'records': records,
         })
 
 
@@ -17,13 +18,27 @@ class UserView(View):
     def get(self, request, username):
         records = Blog_record.objects.filter(author__username=username).order_by('-creation_date')
         return render(request, 'blog/user_page.html', {
-            'records': records
+            'records': records,
         })
 
 
-class SubscriptionsView(View):
+class SubscriptionsView(LoginRequiredMixin, View):
     def get(self, request):
-        return HttpResponse('Current user subscriptions')
+        current_user = User.objects.get(username=request.user.get_username())
+        users = current_user.subscriptions.all()
+        records = Blog_record.objects.filter(author__in=users)
+        return render(request, 'blog/subscriptions_page.html', {
+            'records': records,
+        })
+
+
+class BlogView(LoginRequiredMixin, View):
+    def get(self, request):
+        current_user = User.objects.get(username=request.user.get_username())
+        records = Blog_record.objects.filter(author=current_user)
+        return render(request, 'blog/blog.html', {
+            'records': records,
+        })
 
 
 def index(request):
