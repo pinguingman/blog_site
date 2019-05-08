c70b3aef5c2c0be526783fe27f04010359080788
pinguingman <pinguingman@gmail.com>
2019-05-08 15:55:52 +0300

    Add email sending using gmail SMTP.

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-from .secret import MY_SECRET_KEY
+from .secret import MY_SECRET_KEY, EMAIL_PASSWORD, EMAIL_USER
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -107,6 +107,16 @@ AUTH_USER_MODEL = 'blog.User'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+
+# Email settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = EMAIL_USER
+EMAIL_HOST_PASSWORD = EMAIL_PASSWORD
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
