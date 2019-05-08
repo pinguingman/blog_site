6e16b9fc72f66a772b5f4cca390c8b0c2da59d33
pinguingman <pinguingman@gmail.com>
2019-05-08 18:11:44 +0300

    Add post form for user to create messages

---
+++ blog/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+class MessageForm(forms.Form):
+    title = forms.CharField(
+        max_length=100,
+        label='Заголовок сообщения.',
+        error_messages={'required': 'Вы должны ввести заголовок сообщения.'}
+    )
+    text = forms.CharField(
+        max_length=1000,
+        label='Текст сообщения.',
+        widget=forms.Textarea(attrs={'rows': 4, 'cols': 30}),
+        error_messages={'required': 'Вы должны ввести текст сообщения.'}
+    )
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, TemplateView, View
 
 from blog.models import Blog_record, User
+from .forms import MessageForm
 
 
 class IndexView(View):
@@ -26,7 +28,7 @@ class SubscriptionsView(LoginRequiredMixin, View):
     def get(self, request):
         current_user = User.objects.get(username=request.user.get_username())
         users = current_user.subscriptions.all()
-        records = Blog_record.objects.filter(author__in=users)
+        records = Blog_record.objects.filter(author__in=users).order_by('-creation_date')
         return render(request, 'blog/subscriptions_page.html', {
             'records': records,
         })
@@ -35,10 +37,24 @@ class SubscriptionsView(LoginRequiredMixin, View):
 class BlogView(LoginRequiredMixin, View):
     def get(self, request):
         current_user = User.objects.get(username=request.user.get_username())
-        records = Blog_record.objects.filter(author=current_user)
+        records = Blog_record.objects.filter(author=current_user).order_by('-creation_date')
+        form = MessageForm()
         return render(request, 'blog/blog.html', {
             'records': records,
+            'form': form
         })
+
+    def post(self, request):
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            author = User.objects.get(username=request.user.get_username())
+            new_blog_record = Blog_record(
+                title=form.cleaned_data['title'],
+                text=form.cleaned_data['text'],
+                author=author
+            )
+            new_blog_record.save()
+        return HttpResponseRedirect(reverse('blog:user_blog'))
 
 
 def index(request):
